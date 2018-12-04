9e17316d796a5a5d1de9e4d04dbf5a2aa00cf31b
Helen Mary Labao-Barrameda <helenmary.barrameda@uap.asia>
2018-12-04 21:05:32 +0800

    Edited import syntax

--- nltkbook/bigramstrigrams.py
+++ nltkbook/bigramstrigrams.py
@@ -1,4 +1,6 @@
-from nltk import bigrams, trigrams, word_tokenize text = "John works at Intel."
+from nltk import bigrams, trigrams, word_tokenize 
+
+text = "John works at Intel."
 tokens = word_tokenize(text)
 print(list(bigrams(tokens))) # the `bigrams` function returns a generator, so we must unwind it
 # [('John', 'works'), ('works', 'at'), ('at', 'Intel'), ('Intel', '.')]
